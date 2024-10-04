5674c6aceb4ee88fbad1b0ee05aa130eff535e68
Kevin Tarrant <kmtarrant13@gmail.com>
2024-10-04 01:41:05 -0400

    add locations parser

---
+++ convert/locations.py
@@ -0,0 +1,121 @@
+import os
+import re
+from convert import csv_dir
+
+level_re = re.compile(".*[0-9]F.*")
+grass_encounters_file = "Pokémon Unbound Location Guide v2.1.1.1 - Grass & Cave Encounters.csv"
+water_encounters_file = "Pokémon Unbound Location Guide v2.1.1.1 - Surfing, Fishing, Rock Smash.csv"
+
+
+def parse_encounters_csv(csv_file, skip_lines=0):
+    with open(csv_file, 'r') as f:
+        raw_data = {}
+        skip_index = 0
+        for line in f.readlines():
+            if skip_index < skip_lines:
+                skip_index += 1
+                continue
+            values = line.split(",,")
+            if not raw_data:
+                raw_data = {header: [] for header in values}
+                continue
+
+            for header, value in zip(raw_data, values):
+                raw_data[header].append(value)
+    return raw_data
+
+
+def check_area(value):
+    if value in ["Inside", "Outside"]:
+        return True
+    if "Area" in value:
+        return True
+    if "Flowers" in value:
+        return True
+    if level_re.match(value):
+        return True
+    if value == "Shadow Basement":
+        return True
+    if ", " in value:
+        return True
+    if "Headbutt" in value:
+        return True
+    if value in ["Swarm", "Special Encounter"]:
+        return True
+    if value in ["Easy", "Medium", "Hard", "Insane"]:
+        return True
+    return False
+
+
+def parse_land_columns(raw_data):
+    data = {}
+    for header in raw_data:
+        data[header] = {}
+        data[header]["land"] = {}
+        column = raw_data[header]
+        sub_header = ""
+        sub_column = []
+        for value in column:
+            value = value.strip()
+            if check_area(value):
+                data[header]["land"][sub_header] = sub_column
+                sub_header = value
+                sub_column = []
+                continue
+            if value:
+                sub_column.append(value)
+        data[header]["land"][sub_header] = sub_column
+    return data
+
+
+def parse_water_columns(raw_data):
+    data = {}
+    for header in raw_data:
+        data[header] = {}
+        sheet = "surf"
+        data[header][sheet] = {}
+        column = raw_data[header]
+        sub_header = ""
+        sub_column = []
+        for value in column:
+            value = value.strip()
+            if check_area(value):
+                if sub_column:
+                    data[header][sheet][sub_header] = sub_column
+                sub_header = value
+                sub_column = []
+                continue
+            if "Rod" in value or value == "Fishing" or value == "Rock Smash":
+                if sub_column:
+                    data[header][sheet][sub_header] = sub_column
+                sheet = value
+                data[header][sheet] = {}
+                sub_header = ""
+                sub_column = []
+                continue
+            if value and value != "X":
+                sub_column.append(value)
+        data[header][sheet][sub_header] = sub_column
+    return data
+
+
+def parse_location_files():
+    land_data = parse_land_columns(
+        parse_encounters_csv(os.path.join(csv_dir, grass_encounters_file)),
+    )
+    water_data = parse_water_columns(
+        parse_encounters_csv(os.path.join(csv_dir, water_encounters_file),
+                             skip_lines=2),
+    )
+    for route in land_data:
+        if route in water_data:
+            for key in water_data[route]:
+                land_data[route][key] = water_data[route][key]
+    return land_data
+
+
+if __name__ == "__main__":
+    from convert import pprint_top_entries
+
+    location_data = parse_location_files()
+    pprint_top_entries(location_data)
